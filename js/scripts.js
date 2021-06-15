cdff2547eca7b8ca0a0f5a07d1ab17b3c35d830f
Emmanuel Carmona <pinombole@gmail.com>
2021-06-15 02:11:35 -0300

    final desafio 5

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,83 @@
+class Animacion {
+    constructor(tipo, duracion, tarifa) {
+        this.tipoAnimacion = tipo;
+        this.duracion = duracion;
+        this.tipoTarifa = tarifa;
+    }
 
-let tipoAnimacion;
-let duracionMin;
-let duracionSeg;
-let precioSegAnimacion;
-let tipoCliente;
-let alcance;
-let estiloArte;
+    cotizarTiempoAnimacion() {
+        let precioMinutoAnimacion;
 
-do {
-    tipoAnimacion = prompt("Elija el Tipo de Animación escribiendo Motion Graphics', 'Explicativo Simple', 'Explicativo Complejo', 'Musical Simple', 'Musical Complejo' o 'Narrativo Simple' o 'Narrativo Complejo").toLowerCase();
-    duracionMin = Number(prompt("Ingrese la Duracion en Minutos"));
-    duracionSeg = Number(prompt("Ingrese la Duracion en Segundos"));
-    validacion = !isNaN(duracionMin) && !isNaN(duracionSeg);
-} while (!validacion)
+        if (this.tipoTarifa == "baja") {
+            precioMinutoAnimacion = 150;
+        } else if (this.tipoTarifa == "media") {
+            precioMinutoAnimacion = 300;
+        } else if (this.tipoTarifa == "alta") {
+            precioMinutoAnimacion = 600;
+        } else {
+            alert("Error Cotización")
+        }
 
-const tarifaBaja = tipoAnimacion === "motion graphics" || tipoAnimacion === "explicativo simple";
-const tarifaMedia = tipoAnimacion === "musical simple" || tipoAnimacion === "narrativo simple";
-const tarifaAlta = tipoAnimacion === "explicativo complejo" || tipoAnimacion === "musical complejo" || tipoAnimacion === "narrativo complejo";
+        const precioMinutoPesos = conversionDolarPeso(precioMinutoAnimacion, 94.93);
+        const brutoAnimDolar = precioMinutoAnimacion * this.duracion;
+        const brutoAnimPesos = conversionDolarPeso(brutoAnimDolar, 94.93);
+        const precioMasIva = calcularIva(brutoAnimPesos);
 
-console.log(tarifaBaja);
-console.log(tarifaMedia);
-console.log(tarifaAlta);
-
-//No sé si esto conviene que sea una funcion o queda acá suelto. 
-//Es para definir que tipo de tarifa segun lo que elija el Usuario
-if (tarifaBaja) {
-    precioSegAnimacion = 2.5;
-} else if (tarifaMedia) {
-    precioSegAnimacion = 5;
-} else if (tarifaAlta) {
-    precioSegAnimacion = 10;
-} else {
-    alert("Error Cotización. Ingrese Tipo de Animacion Correctamente")
+        console.log(`El costo del minuto de animacion de un ${this.tipoAnimacion} es de ${precioMinutoAnimacion} dólares o ${precioMinutoPesos} pesos. El precio total de la animación es ${brutoAnimDolar} dolares o ${brutoAnimPesos} pesos. El precio con IVA es ${precioMasIva} pesos`);
+    }
 }
-// console.log(precioSegAnimacion);
 
+class Cliente {
+    constructor(nombre, mail, pais, tipoCliente, alcance) {
+        this.nombre = nombre;
+        this.mail = mail;
+        this.mail = pais;
+        this.mail = tipoCliente;
+        this.alcance = alcance;
+    }
+}
+
+const secondsToMinutes = (segundos) => segundos / 60;
 const minutesToSeconds = (minutos) => minutos * 60;
 const multiplicar = (x, y) => x * y;
 
 function conversionDolarPeso(dolar, cotizacion) {
     let tarifaPesos = dolar * cotizacion;
-    return tarifaPesos.toFixed(2);
+    return tarifaPesos.toFixed(0);
 }
 
 function calcularIva(bruto) {
     let iva = bruto * 1.21;
-    return iva.toFixed(2);
+    return iva.toFixed(0);
 }
 
-const precioMinutoAnimacion = multiplicar(precioSegAnimacion, 60);
-const precioMinutoPesos = conversionDolarPeso(precioMinutoAnimacion, 94.93);
+do {
+    duracionMin = Number(prompt("Ingrese la Duracion en Minutos"));
+    duracionSeg = Number(prompt("Ingrese la Duracion en Segundos"));
+    validacion = !isNaN(duracionMin) && !isNaN(duracionSeg);
+} while (!validacion)
 
-const duracionAnimacion = minutesToSeconds(duracionMin) + duracionSeg;
-// console.log(duracionAnimacion);
+const duracionIngresada = secondsToMinutes(duracionSeg) + duracionMin;
 
-const brutoAnimDolar = precioSegAnimacion * duracionAnimacion;
-const brutoAnimPesos = conversionDolarPeso(brutoAnimDolar, 94.93);
+const motionGraphics = new Animacion("Motion Graphics", duracionIngresada, "baja");
+const explicativoSimple = new Animacion("Video Explicativo Simple", duracionIngresada, "baja");
+const musicalSimple = new Animacion("Video Musical Simple", duracionIngresada, "media");
+const narrativoSimple = new Animacion("Video Narrativo Simple", duracionIngresada, "media");
+const explicativoComplejo = new Animacion("Video Explicativo Complejo", duracionIngresada, "alta");
+const musicalComplejo = new Animacion("Video Musical Complejo", duracionIngresada, "alta");
+const narrativoComplejo = new Animacion("Video Narrativo Complejo", duracionIngresada, "alta");
 
-const precioMasIva = calcularIva(brutoAnimPesos);
 
-console.log(`El precio del Minuto de Animación es ${precioMinutoAnimacion} dolares o ${precioMinutoPesos} pesos. El precio total de la animación es ${brutoAnimDolar} dolares o ${brutoAnimPesos} pesos. El precio con IVA es ${precioMasIva} pesos`);
-alert(`El precio del Minuto de Animación es ${precioMinutoAnimacion} dolares o ${precioMinutoPesos} pesos. El precio total de la animación es ${brutoAnimDolar} dolares o ${brutoAnimPesos} pesos. El precio con IVA es ${precioMasIva} pesos`);
-//Hasta acá el Desafio 4 y Entrega del Simulador Interactivo
+
+motionGraphics.cotizarTiempoAnimacion();
+explicativoSimple.cotizarTiempoAnimacion();
+musicalSimple.cotizarTiempoAnimacion();
+narrativoSimple.cotizarTiempoAnimacion();
+explicativoComplejo.cotizarTiempoAnimacion();
+musicalComplejo.cotizarTiempoAnimacion();
+narrativoComplejo.cotizarTiempoAnimacion();
+// //Hasta acá el Desafio 5, incorporando Objects a mi Simulador
+
 
 
 
